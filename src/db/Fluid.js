ef6c0598989b259454ebe94298aa6e0275e8210f
NickJApps <nicolasfr74@example.com>
2026-08-16 15:35:59 +0300

    fluid-card-filling

--- src/db/Fluid.js
+++ src/db/Fluid.js
@@ -6,6 +6,9 @@ export const fluid = [
         partNumber: "A22L",
         name: "Brake Fluid",
         type: "DOT-4",
+        description: "Synthetic glycol-based brake fluid with oxidation and corrosion inhibitors for hydraulic brake and clutch systems.",
+        compatibility: "FMVSS 116 DOT 4, SAE J 1703/1704, ISO 4925 Class 3/4/6, JIS K2233 Class 3/4/6Ford WSS-M6C65-A2, VW 501 14",
+        volumen: "1L",
         price: 150,
         image: "/products/fluid/fluid-1.jpg"
 
@@ -17,6 +20,9 @@ export const fluid = [
         partNumber: "A22LS",
         name: "Brake Fluid",
         type: "DOT-3",
+        description: "Synthetic glycol-based brake fluid with oxidation and corrosion inhibitors for hydraulic brake and clutch systems.",
+        compatibility: "FMVSS 116 DOT 4, SAE J 1703/1704, ISO 4925 Class 3/4/6, JIS K2233 Class 3/4/6Ford WSS-M6C65-A2, VW 501 14",
+        volumen: "250ML",
         price: 50,
         image: "/products/tools/fluid-2.jpg"
 
